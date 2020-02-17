70823ea656025d7f0c41571e1c532b5ed8f64950
Evan Allrich <evan@unguku.com>
2020-02-17 15:27:07 -0600

    Support redis via a full URL, not just hostname

--- server/storage/redis.js
+++ server/storage/redis.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
   //eslint-disable-next-line security/detect-non-literal-require
   const redis = require(redis_lib);
   const client = redis.createClient({
-    host: config.redis_host,
+    url: config.redis_host,
     retry_strategy: options => {
       if (options.total_retry_time > 10000) {
         client.emit('error', 'Retry time exhausted');
